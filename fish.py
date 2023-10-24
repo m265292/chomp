ff21b6705d717181284a5be175a2d1f17a4bbfde
m265292 <m265292@usna.edu>
2023-10-24 08:25:01 -0400

    create game

--- fish.py
+++ fish.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import random
 import pygame
 class Fish():
@@ -17,4 +18,25 @@ class Fish():
 
     def draw(self):
         # blit the fish on the screen
+=======
+import random
+import pygame
+class Fish():
+    def __init__(self,screen):
+        self.image = pygame.image.load('assets/images/fishTile_079.png')
+        self.image = pygame.transform.flip(self.image, 1,0)
+        self.speed = random.random() * 5
+        self.x = screen.get_width()
+        self.y = random.randint(0,screen.get_height())
+        self.screen = screen
+
+    def update(self):
+        # move the fish to the left
+        self.x -=self.speed
+        if self.x<0:
+            self.x = self.screen.get_width()
+
+    def draw(self):
+        # blit the fish on the screen
+>>>>>>> Stashed changes
         self.screen.blit(self.image, (self.x,self.y))
